33613842a6a0d2a0abe7e33fb8d5e554ca02e719
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-02-15 16:08:34 -0800

    update file 11 script

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,7 @@
--- updates teh score of Bob to 10 int the 
--- second table
--- using only the name field
-UPDATE second_table SET score = 10 WHERE name = 'Bob';
+USE hbtn_0c_0;
+
+-- List records with a score >= 10, ordered by score (top first)
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
