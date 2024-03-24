8b9b7df876b9db0672f222872598bbf6d677439c
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 12:03:59 -0700

    javascript objects

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+
+// Importing the list array from the file 100-data.js
+const { list } = require('./100-data');
+
+// Creating a new array using map, where each element is the value of the initial list multiplied by its index
+const newList = list.map((value, index) => value * index);
+
+// Printing both the initial list and the new list
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newList);
+
