93cf642c46e735d93ccd6d1593e16ad9d7a36298
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 11:15:00 -0700

    integer fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-const argv = process.argv;
-const num = parseInt(argv[2]);
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
+
+// Parse the first argument as an integer
+const num = parseInt(process.argv[2]);
+
+// Check if the parsed number is a valid integer
+if (!isNaN(num) && Number.isInteger(num)) {
+  // If it's a valid integer, print "My number: <integer>"
   console.log(`My number: ${num}`);
+} else {
+  // If it's not a valid integer, print "Not a number"
+  console.log("Not a number");
 }
+
