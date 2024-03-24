b931bc8ca073816491b31f154764eb4eaec26201
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 10:51:11 -0700

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
-const argv = process.argv;
-console.log(`${argv[2]} is ${argv[3]}`);
+
+// Check if both arguments are passed to the script
+if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
+  // If both arguments are passed, print the concatenated string
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else {
+  // If one or both arguments are missing, print "undefined is undefined" or "<argument> is undefined"
+  console.log(`${process.argv[2] || 'undefined'} is ${process.argv[3] || 'undefined'}`);
+}
+
