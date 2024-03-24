519cb82f0e6773fecf0309e81766037aefe9a957
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 10:42:22 -0700

    few fixes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+
+// Use console.log(...) to print the first line: "C is fun"
+console.log("C is fun");
+
+// Use console.log(...) to print the second line: "Python is cool"
+console.log("Python is cool");
+
+// Use console.log(...) to print the third line: "JavaScript is amazing"
+console.log("JavaScript is amazing");
+
