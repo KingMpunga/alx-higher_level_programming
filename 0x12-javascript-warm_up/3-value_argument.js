555adb0c5f7bf8efe6697c8b710153519fcd6a73
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 10:47:28 -0700

    value argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv[2] === undefined) {
-  console.log('No argument');
+
+// Check if any argument is passed to the script
+if (process.argv[2] === undefined) {
+  // If no arguments are passed, print "No argument"
+  console.log("No argument");
 } else {
-  console.log(argv[2]);
+  // If an argument is passed, print the first argument
+  console.log(process.argv[2]);
 }
+
