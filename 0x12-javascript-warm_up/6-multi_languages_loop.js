550fbb06e8f7f3eae015dc3bb52aea9bd9b57197
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 11:17:52 -0700

    multi language loop fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+
+// Define an array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a for loop to iterate over the array and print each element
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
 }
+
