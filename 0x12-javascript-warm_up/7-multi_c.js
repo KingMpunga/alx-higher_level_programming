8f81487c26a3ad21d7e62d7777671109e4d56624
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 11:20:55 -0700

    multi file fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-const argv = process.argv;
-const x = parseInt(argv[2]);
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
+
+// Parse the first argument as an integer
+const x = parseInt(process.argv[2]);
+
+// Check if the parsed number is a valid integer
+if (!isNaN(x) && Number.isInteger(x) && x > 0) {
+  // Use a for loop to print "C is fun" x times
   for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+    console.log("C is fun");
   }
+} else {
+  // If the argument is missing or not a positive integer, print "Missing number of occurrences"
+  console.log("Missing number of occurrences");
 }
+
