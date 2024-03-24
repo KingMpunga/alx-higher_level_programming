fe47fe4d4609701acdabe8b753492222973dce54
Bukusu Jean <mpunga.djodjo@gmail.com>
2024-03-24 11:23:45 -0700

    square file fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,20 @@
 #!/usr/bin/node
-const argv = process.argv;
-const x = parseInt(argv[2]);
-const character = 'X';
-if (isNaN(x)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log(character.repeat(x));
+
+// Parse the first argument as an integer
+const size = parseInt(process.argv[2]);
+
+// Check if the parsed number is a valid positive integer
+if (!isNaN(size) && Number.isInteger(size) && size > 0) {
+  // Use a nested loop to print the square
+  for (let i = 0; i < size; i++) {
+    let row = "";
+    for (let j = 0; j < size; j++) {
+      row += "X";
+    }
+    console.log(row);
   }
+} else {
+  // If the argument is missing or not a positive integer, print "Missing size"
+  console.log("Missing size");
 }
+
